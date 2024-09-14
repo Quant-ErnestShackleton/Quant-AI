57c0544cb3892cfe00a9a851aaa145bc4fd71fae
junhaa <2171326@hansung.ac.kr>
2024-09-15 06:44:27 +0900

    :sparkles: feat. XGBoost 모델 구현

---
+++ src/model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def train_model():
+
+    #CSV 파일 불러오기
+    df = pd.read_csv("./src/dutch_pay_grouped_data_corrected.csv")
+
+    X = df.drop('label', axis=1)
+    y = df['label']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = xgb.XGBClassifier(n_estimators=100, max_depth=3, learning_rate=0.1, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"테스트 결과 정확도 : {accuracy * 100:.2f}%")
