567cff6be39052876a36a3a1102e8b82fd8c599e
junhaa <2171326@hansung.ac.kr>
2024-09-15 06:48:29 +0900

    :sparkles: CHORE. main.py 실행 코드 변경

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # main.py
+from src.model import train_model
+from src.createData import createData
 
 if __name__ == "__main__":
-    print("Hello world!")
+    #createData() # 학습 데이터 생성
+    train_model() # 모델 학습
