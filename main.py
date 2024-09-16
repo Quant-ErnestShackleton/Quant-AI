aa951d12d85fc46f47d5516e1ba10565cbf269d3
junhaa <2171326@hansung.ac.kr>
2024-09-17 03:29:06 +0900

    :sparkles: FEAT. 평탄화 및 패딩 작업 후 학습 코드 구현

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # main.py
-from src.model import train_model
-from src.createData import createData
+from src.train import process_and_train_xgboost
+# from src.createData import createData
 from src.dataToJson import data_to_json
 
+# 함수 호출 예시
+dutchpay_csv = './data/generated_dutch_pay_data.csv' # dutchpay csv 파일 경로
+non_dutchpay_csv = './data/generated_non_dutch_pay_data.csv' # non-dutchpay csv 파일 경로
+
 if __name__ == "__main__":
-    createData(500, 500)
-    # train_model() # 모델 학습
-    # data_to_json()
+    # createData(100000, 100000)
+
+    # 모델 학습
+    model, X_test, y_test, y_pred, accuracy, report = process_and_train_xgboost(
+        dutchpay_csv_path=dutchpay_csv,
+        non_dutchpay_csv_path=non_dutchpay_csv,
+        max_participants=50,
+        test_size=0.2,
+        random_state=42,
+        apply_smote=True  # 클래스 불균형 처리 여부
+    )
