1eb9e329d7eab66163548bb0de4e19e512fc8f00
junhaa <2171326@hansung.ac.kr>
2024-09-16 20:38:54 +0900

    :art: CHORE. 코드 구조 개선

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # main.py
 from src.model import train_model
-from src.createData import createDutchData, createNonDutchData
+from src.createData import createData
 from src.dataToJson import data_to_json
 
 if __name__ == "__main__":
-    # createDutchData(500) # 학습 데이터 생성
-    # createNonDutchData(500)
+    createData(500, 500)
     # train_model() # 모델 학습
-    data_to_json()
+    # data_to_json()
